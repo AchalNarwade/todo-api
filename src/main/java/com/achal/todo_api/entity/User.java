63ba1bd00b6e386c5fe2e176528360263fca9d95
AchalNarwade <achalnarwade211@gmail.com>
2026-06-30 11:43:05 +0530

    Add user entity

---
+++ src/main/java/com/achal/todo_api/entity/User.java
@@ -0,0 +1,37 @@
+package com.achal.todo_api.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+}
