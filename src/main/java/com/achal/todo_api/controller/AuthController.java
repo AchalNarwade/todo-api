7daceb6cdb2ebe40d4489a4de912c14c45ecf3d4
AchalNarwade <achalnarwade211@gmail.com>
2026-07-22 00:26:25 +0530

    Add Swagger annotations for API documentation

--- src/main/java/com/achal/todo_api/controller/AuthController.java
+++ src/main/java/com/achal/todo_api/controller/AuthController.java
@@ -5,6 +5,10 @@ import com.achal.todo_api.dto.auth.LoginResponse;
 import com.achal.todo_api.dto.auth.RegisterRequest;
 import com.achal.todo_api.dto.auth.RegisterResponse;
 import com.achal.todo_api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name = "Authentication",description = "User Registration and login APIs")
 public class AuthController {
 
     private final UserService userService;
@@ -21,11 +26,28 @@ public class AuthController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Register a new user",
+            description = "Create a new user account."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201",description = "User registered successfully"),
+            @ApiResponse(responseCode = "400",description = "Validation default"),
+            @ApiResponse(responseCode = "409",description = "User already exists")
+    })
     @PostMapping("/register")
     public RegisterResponse register(@Valid @RequestBody RegisterRequest request){
         return userService.register(request);
     }
 
+    @Operation(
+            summary = "Login user",
+            description = "Authenticates a user and returns a JWT token"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Login successful"),
+            @ApiResponse(responseCode = "401",description = "Invalid email or password")
+    })
     @PostMapping("/login")
     public LoginResponse login(@Valid @RequestBody LoginRequest loginRequest){
         return userService.login(loginRequest);
--- src/main/java/com/achal/todo_api/controller/TaskController.java
+++ src/main/java/com/achal/todo_api/controller/TaskController.java
@@ -5,6 +5,10 @@ import com.achal.todo_api.dto.task.CreateTaskRequest;
 import com.achal.todo_api.dto.task.TaskResponse;
 import com.achal.todo_api.dto.task.UpdateTaskRequest;
 import com.achal.todo_api.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
+@Tag(name = "Tasks",description = "Task Management APIs")
 public class TaskController {
 
     //dependency injection
@@ -22,26 +27,72 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation(
+            summary = "Create a task",
+            description = "Creates a new task for authenticated user."
+    )
     @PostMapping
     public TaskResponse createTask(@Valid @RequestBody CreateTaskRequest createTaskRequest){
         return taskService.createTask(createTaskRequest);
     }
+
+    @Operation(
+            summary = "Get all tasks",
+            description = "Return all tasks belonging to the authenticated user."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Tasks retrieved successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @GetMapping
     public List<TaskResponse> getAllTasks(){
         return taskService.getAllTasks();
     }
 
+
+
+    @Operation(
+            summary = "Get task by ID",
+            description = "Return a specific task owned by the authenticated user."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Task found"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "Task not found")
+    })
     @GetMapping("/{id}")
     public TaskResponse getTaskById(@PathVariable Long id){
         return taskService.getTaskById(id);
     }
 
+
+
+    @Operation(
+            summary = "Update task",
+            description = "Updates an existing task."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Task created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @PutMapping("/{id}")
     public TaskResponse updateTask(@PathVariable Long id,
                                    @Valid @RequestBody UpdateTaskRequest updateTaskRequest){
         return taskService.updateTask(id,updateTaskRequest);
     }
 
+
+
+    @Operation(
+            summary = "Delete task",
+            description = "Deletes a task by its ID."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Task updated successfully"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "Task not found")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteTask(@PathVariable Long id){
         taskService.deleteTask(id);
